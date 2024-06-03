d38bddadddca97323b14f53d7bb0c2285a9bdad6
Fabian Friedrich <friedrich.fabian@gmx.net>
2024-06-03 22:10:37 +0200

    Tried out Animations of Nicolas

--- scenes/rotate_cam.gd
+++ scenes/rotate_cam.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
+@onready var anim_player = $AnimationPlayer
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate_y(.2 * delta)
+	anim_player.play("walk")
+	#rotate_y(.5 * delta)
 	pass
