d4d40362df3ae3d20e976f607ead1b82885df04a
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-05-26 16:35:55 +0200

    Update to godot 4.4 stable

--- scenes/rotate_cam.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-@onready var anim_player = $AnimationPlayer
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	anim_player.play("walk")
-	rotate_y(.5 * delta)
-	pass
--- scripts/CharacterBody3D.gd
+++ scripts/CharacterBody3D.gd
@@ -5,20 +5,45 @@ const ACCEL = 10
 
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
 @export var target: Marker3D
+@onready var anim_player = $nicolas/AnimationPlayer
+var camera : Camera3D
 
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	nav.target_position = target.global_position
+	camera = get_viewport().get_camera_3d()
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		var mouse_pos = event.position
+		var from = camera.project_ray_origin(mouse_pos)
+		var to = from + camera.project_ray_normal(mouse_pos) * 1000
+		var space_state = get_world_3d().direct_space_state
+		var params = PhysicsRayQueryParameters3D.create(from, to)
+		params.exclude = [self]
+		
+		var result = space_state.intersect_ray(params)
+
+		if result:
+			target.global_position = result.position
+			nav.target_position = result.position
 
 func _physics_process(delta):
-	print(nav.is_target_reached())
 	if not nav.is_target_reached():
+		anim_player.play("walk")
 		var direction = nav.get_next_path_position() - global_position
 		direction = direction.normalized()
 		
-		look_at(global_transform.origin + direction, Vector3.UP)
-	
+		var flat_direction = nav.get_next_path_position() - global_position
+		flat_direction.y = 0
+		flat_direction = flat_direction.normalized()
+		if flat_direction.length() > 0:
+			look_at(global_transform.origin - flat_direction, Vector3.UP)
+
 		velocity = velocity.lerp(direction * SPEED, ACCEL * delta)
-	else: 
+	else:
+		anim_player.play("idle")
 		velocity = Vector3.ZERO
 	
 	if not is_on_floor():
