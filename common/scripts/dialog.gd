4b6bc2d2bfde6b3c27fafde8ab6430bf7131d298
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-05-27 21:25:16 +0200

    Add: dialogic plugin

--- common/scripts/dialog.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name Dialog
-
-@export var dialog_title: String
-@export var dialog_text: String
-@export var next_dialog: Dialog
--- common/scripts/interactable.gd
+++ common/scripts/interactable.gd
@@ -1,10 +1,29 @@
 extends Area3D
 
 @onready var mesh: MeshInstance3D = $MeshInstance3D 
-var outline_mat = load("res://scenes/common/outline.tres")
+var outline_mat = load("res://common/shader/outline.tres")
+var mouse_on_entity: bool = false
+
+enum OnClickEvent { StartDialogue, TriggerSignal }
+@export var on_click_event: OnClickEvent
+@export var timeline: String 
+
+func _unhandled_input(event):
+	var left_click = event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed
+	if not left_click or not mouse_on_entity:
+		return 
+
+	match on_click_event: 
+		OnClickEvent.StartDialogue: 
+			Dialogic.start(timeline)
+		OnClickEvent.TriggerSignal: 
+			# SignalBus.
+			pass
 
 func _on_mouse_entered() -> void:
 	mesh.material_overlay = outline_mat
+	mouse_on_entity = true
 	
 func _on_mouse_exited() -> void:
 	mesh.material_overlay = null
+	mouse_on_entity = false
